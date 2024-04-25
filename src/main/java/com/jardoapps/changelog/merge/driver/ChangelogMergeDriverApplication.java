e771d90ddfe8db587153921e987192b4e165ce53
Jaroslav Brtis <jaroslav.brtis@gmail.com>
2024-04-25 17:27:04 -0400

    Loading changelog files.

--- src/main/java/com/jardoapps/changelog/merge/driver/ChangelogMergeDriverApplication.java
+++ src/main/java/com/jardoapps/changelog/merge/driver/ChangelogMergeDriverApplication.java
@@ -1,8 +1,14 @@
 package com.jardoapps.changelog.merge.driver;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 public class ChangelogMergeDriverApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 
 		if (args.length < 3) {
 			System.err.println("Expected 3 arguments, but found " + args.length);
@@ -17,8 +23,19 @@ public class ChangelogMergeDriverApplication {
 
 		System.out.println(String.format("Changelog merge. Ours: %s, original: %s, their: %s", ourFile, originalFile, theirFile));
 
-		// TODO: parse and merge changelogs
+		Changelog ourChangelog = loadChangelog(ourFile);
+		Changelog theirChangelog = loadChangelog(theirFile);
+
+		// TODO: merge changelogs
 
 	}
 
+	private static Changelog loadChangelog(String path) throws IOException {
+
+		try (BufferedReader reader = Files.newBufferedReader(Path.of(path), StandardCharsets.UTF_8)) {
+			ChangelogParser parser = new ChangelogParser();
+			parser.parse(reader);
+			return parser.getChangelog();
+		}
+	}
 }

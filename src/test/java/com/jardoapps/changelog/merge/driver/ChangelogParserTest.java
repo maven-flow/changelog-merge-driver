150fc22d8569aceeb826ee3e67277c75b7323a02
Jaroslav Brtis <jaroslav.brtis@gmail.com>
2024-04-25 21:19:48 -0400

    Changelog content added directly instead of reading a file.

--- src/test/java/com/jardoapps/changelog/merge/driver/ChangelogParserTest.java
+++ src/test/java/com/jardoapps/changelog/merge/driver/ChangelogParserTest.java
@@ -3,8 +3,7 @@ package com.jardoapps.changelog.merge.driver;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.StringReader;
 
 import org.junit.jupiter.api.Test;
 
@@ -18,11 +17,8 @@ class ChangelogParserTest {
 
 		ChangelogParser parser = new ChangelogParser();
 
-		ClassLoader classLoader = getClass().getClassLoader();
-		try (InputStream inputStream = classLoader.getResourceAsStream("files/changelog-parser-test/changelog.md")) {
-			try (BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream))) {
-				parser.parse(reader);
-			}
+		try (BufferedReader reader = new BufferedReader(new StringReader(CHANGELOG_TO_PARSE))) {
+			parser.parse(reader);
 		}
 
 		Changelog changelog = parser.getChangelog();
@@ -55,7 +51,7 @@ class ChangelogParserTest {
 		assertThat(section.getLines()).containsExactly(
 				"",
 				"- Use frontmatter title & description in each language version template",
-				"- Replace broken OpenGraph image with an appropriately-sized Keep a Changelog ",
+				"- Replace broken OpenGraph image with an appropriately-sized Keep a Changelog",
 				"  image that will render properly (although in English for all languages)",
 				""
 		);
@@ -64,7 +60,7 @@ class ChangelogParserTest {
 		assertThat(section.getName()).isEqualTo("Removed");
 		assertThat(section.getLines()).containsExactly(
 				"",
-				"- Trademark sign previously shown after the project description in version ",
+				"- Trademark sign previously shown after the project description in version",
 				"0.3.0",
 				""
 		);
@@ -131,4 +127,63 @@ class ChangelogParserTest {
 
 	}
 
+	private static final String CHANGELOG_TO_PARSE = """
+			# Changelog
+
+			All notable changes to this project will be documented in this file.
+
+			The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.1.0/),
+			and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
+
+			## [1.2.0] - SNAPSHOT
+
+			### Added
+
+			- v1.1 Brazilian Portuguese translation.
+			- v1.1 German Translation
+
+			### Changed
+
+			- Use frontmatter title & description in each language version template
+			- Replace broken OpenGraph image with an appropriately-sized Keep a Changelog
+			  image that will render properly (although in English for all languages)
+
+			### Removed
+
+			- Trademark sign previously shown after the project description in version
+			0.3.0
+
+			## [1.1.1] - 2023-03-05
+
+			### Added
+
+			- Arabic translation (#444).
+			- v1.1 French translation.
+
+			### Fixed
+
+			- Improve French translation (#377).
+			- Improve id-ID translation (#416).
+
+			### Changed
+
+			- Upgrade dependencies: Ruby 3.2.1, Middleman, etc.
+
+			### Removed
+
+			- Unused normalize.css file.
+			- Identical links assigned in each translation file.
+
+			## [1.1.0] - 2019-02-15
+
+			### Added
+
+			- Danish translation (#297).
+			- Georgian translation from (#337).
+
+			### Fixed
+
+			- Italian translation (#332).
+			- Indonesian translation (#336).
+			""";
 }
